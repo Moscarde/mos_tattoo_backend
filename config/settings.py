4e5f51868713bd15bdb2372853de1204d9db28e2
Moscarde <gabrielmoscarde@outlook.com>
2026-02-18 16:09:31 -0300

    fix(settings): update default SECRET_KEY and expand ALLOWED_HOSTS for development

--- config/settings.py
+++ config/settings.py
@@ -25,13 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config(
     "SECRET_KEY",
-    default="django-insecure-f^zmn-gni=$lr^ug(fo%+%%^e=_jpl)%g(+rk*ruzr&#l7tbqn",
+    default="django-insecure-change-in-production",
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS", default="localhost,127.0.0.1,0.0.0.0", cast=Csv()
+)
 
 
 # Application definition
